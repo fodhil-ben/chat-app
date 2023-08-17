35013fb3959240dda29456956b7444c7fa0e65ff
fodhil-ben <benhibafodhil@gmail.com>
2023-08-17 21:38:53 +0100

    Update: updating the UI

--- backend/socket.js
+++ backend/socket.js
@@ -5,7 +5,7 @@ function InitServer(server) {
         // pingTimeout
         cors: {
             // origin: "http://localhost:5173"
-            origin: "https://chat-app-i3zg.onrender.com"
+            origin: "*"
         }
     })
     io.on('connection', (socket) => {
