2f46a0663cd9e8850960bafe6ce0cfdc93d8689b
fodhil-ben <benhibafodhil@gmail.com>
2023-08-17 21:09:04 +0100

    Update: updating the UI

--- backend/db.js
+++ backend/db.js
@@ -4,12 +4,12 @@ dotenv.config();
 
 if (process.env.NODE_ENV === 'production') {
 
-    const client = new Client({
+    var client = new Client({
         connectionString: process.env.POSTGRES_URL + "?sslmode=require",
     })
 }
 else {
-    const client = new Client({
+    var client = new Client({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DATABASE,
