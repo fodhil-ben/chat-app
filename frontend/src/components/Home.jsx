bd8b404c11947cb066c3daada7934bc183e3f7dc
fodhil-ben <benhibafodhil@gmail.com>
2023-08-18 16:49:57 +0100

    Update: Add portfolio url

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
             <div id='home' className='h-screen'>
                 <div id="welcome" className='w-3/5 mx-auto mt-40 p-10 rounded-md'>
                     <h1 className='font-bold text-3xl mb-10'>Welcome to Chat App</h1>
-                    <button className='font-bold px-5 py-3 rounded-xl'>More About Me</button>
+                    <a href="https://fodhil-benhiba.vercel.app"><button className='font-bold px-5 py-3 rounded-xl'>More About Me</button></a>
                 </div>
             </div>
             <footer className='pb-10 flex items-center tracking-widest w-screen justify-center m-auto'>
